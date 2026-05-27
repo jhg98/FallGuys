9e159131d7adfaad8a5fcc477467d56d54a123d4
jhg98 <57996692+jhg98@users.noreply.github.com>
2026-05-27 19:37:46 +0900

    씬 폴더링, 버튼 이미지 변경, 버튼매니저 분리, 버튼 인스펙터 방식에서 AddListener 방식으로 변경

---
+++ Assets/0.MyAssets/01.Scripts/LobbyButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyButtonManager : MonoBehaviour
+{
+    [SerializeField] private Button ModeSelectButton;
+    [SerializeField] private Button PlayButton;
+
+    private void Start()
+    {
+        ModeSelectButton.onClick.AddListener(OnClickModeSelectButton);
+        PlayButton.onClick.AddListener(OnClickPlayButton);
+    }
+    public void OnClickModeSelectButton()
+    {
+        SceneManager.LoadScene("ModeSelect");
+    }
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene("Matching");
+    }
+}
---
+++ Assets/0.MyAssets/01.Scripts/ModeSelectButtonManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ModeSelectButtonManager : MonoBehaviour
+{
+    [SerializeField] private Button PracticeModeButton;
+    [SerializeField] private Button CompetitiveModeButton;
+    [SerializeField] private Button LobbyButton;
+
+    void Start()
+    {
+        PracticeModeButton.onClick.AddListener(OnClickPracticeModeButton);
+        CompetitiveModeButton.onClick.AddListener(OnClickCompetitiveModeButton);
+        LobbyButton.onClick.AddListener(OnClickLobbyButton);
+
+        if(GameSettings.Instance.CurrentPlayMode == GameSettings.PlayMode.Practice)
+        {
+            PracticeModeButton.interactable = false;
+            CompetitiveModeButton.interactable = true;
+        }
+        else if (GameSettings.Instance.CurrentPlayMode == GameSettings.PlayMode.Competitive)
+        {
+            PracticeModeButton.interactable = true;
+            CompetitiveModeButton.interactable = false;
+        }
+    }
+    public void OnClickPracticeModeButton()
+    {
+        Debug.Log("연습 모드 설정");
+
+        PracticeModeButton.interactable = false;
+        CompetitiveModeButton.interactable = true;
+
+        GameSettings.Instance.CurrentPlayMode = GameSettings.PlayMode.Practice;
+        GameSettings.Instance.IsMapRandom = true;
+
+        // 맵 랜덤 또는 선택 로직 추가 예정
+        // 맵 선택 버튼 클릭 시 맵 리스트 활성화 or 맵 리스트에 랜덤까지 넣기
+    }
+    public void OnClickCompetitiveModeButton()
+    {
+        Debug.Log("경쟁 모드 설정");
+
+        PracticeModeButton.interactable = true;
+        CompetitiveModeButton.interactable = false;
+
+        GameSettings.Instance.CurrentPlayMode = GameSettings.PlayMode.Competitive;
+        GameSettings.Instance.IsMapRandom = true;
+    }
+    public void OnClickLobbyButton()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
