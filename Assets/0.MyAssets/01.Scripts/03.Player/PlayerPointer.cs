8681a023683fcfb3de8564490f9923c1420c5117
jhg98 <57996692+jhg98@users.noreply.github.com>
2026-05-28 03:45:27 +0900

    내 캐릭터 표시

---
+++ Assets/0.MyAssets/01.Scripts/03.Player/PlayerPointer.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class PlayerPointer : MonoBehaviourPun
+{
+    [SerializeField] private GameObject PlayerPointerImage;
+
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            PlayerPointerImage.SetActive(true);
+        }
+        else
+        {
+            PlayerPointerImage.SetActive(false);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (photonView.IsMine)
+        {
+            PlayerPointerImage.transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
